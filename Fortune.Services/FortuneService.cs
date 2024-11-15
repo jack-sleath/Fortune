bfa462917ff413fb57433622e52c9575bea21c71
jacksleath <jackwilliamsleath@gmail.com>
2024-11-15 19:38:00 +0000

    helper

--- Fortune.Services/FortuneService.cs
+++ Fortune.Services/FortuneService.cs
@@ -34,7 +34,7 @@ namespace Fortune.Services
         {
             var list = new List<FortuneModel>();
 
-            for (int i = 0; i < 1; i++)
+            for (int i = 0; i < 15; i++)
             {
                 var blah = await GetFortune();
                 list.Add(blah);
