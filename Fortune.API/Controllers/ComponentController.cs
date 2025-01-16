c6a86984131fe3de76cd99ab596ce3d7d833af05
Will White <willwhite84@gmail.com>
2025-01-16 20:57:02 +0000

    Basic implementation of TTS with elevenlabs

---
+++ Fortune.API/Controllers/ComponentController.cs
@@ -0,0 +1,34 @@
+﻿using Fortune.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fortune.API.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class ComponentController : ControllerBase {
+
+        private readonly IExternalImageAiService _imageAIService;
+        private readonly ITtsService _ttsService;
+
+
+        public ComponentController(IExternalImageAiService imageAiService, ITtsService ttsService) {
+            _imageAIService = imageAiService;
+            _ttsService = ttsService;
+        }
+
+        [HttpGet("generateAudio", Name = "GenerateAudioFromText")]
+        public IActionResult GenerateAudioFromText(string text) {
+            
+            var response = _ttsService.GetTTSBlob(text);
+            return StatusCode(200);
+        }
+
+        [HttpGet("generateImage", Name = "GenerateImageFromText")]
+        public IActionResult GenerateImageFromText(string text) {
+
+            var response = _imageAIService.GenerateImageAsync(text);
+            
+            return Ok(response);
+        }
+    }
+}
---
+++ Fortune.Models/Configs/TtsConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fortune.Models.Configs {
+    public class TtsConfig {
+        public string TtsProvider { get; set; }
+        public string TtsVoiceId { get; set; }
+
+        public string ApiKey { get; set; }
+    }
+}
